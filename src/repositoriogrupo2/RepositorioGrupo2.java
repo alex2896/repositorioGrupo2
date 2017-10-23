49a4a487f3b36f89ddd7ebecf264bb42f7f95917
Alexis <>
2017-10-23 09:23:17 -0500

    Primer commit para inicializar el proyecto.

---
+++ src/repositoriogrupo2/RepositorioGrupo2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositoriogrupo2;
+
+/**
+ *
+ * @author Invitado_PC04
+ */
+public class RepositorioGrupo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
